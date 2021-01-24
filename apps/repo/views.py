616badabd4421c257ca075f929b54cb9fb29573f
yuhixyz <byh668972@gmail.com>
2021-01-24 20:22:19 +0800

    完成数据库文件删除功能

--- apps/repo/views.py
+++ apps/repo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from apps.repo.models import FileRepository
+from django.http import JsonResponse
 
-# Create your views here.
+
+def delete_view(request):
+    delete_id = request.GET.get('did')
+    delete_object = FileRepository.objects.filter(id=delete_id, update_user=request.user).first()
+    if delete_object.file_type == 1:  # 删除文件
+        pass
+    else:
+        pass
+
+    delete_object.delete()  # 删除数据库中的文件id=delete_id的文件（夹）
+    return JsonResponse({'status': True})
+
--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from apps.views import index_view
 from apps.account.views import login_view, logout_view
+from apps.repo.views import delete_view
 
 
 urlpatterns = [
@@ -9,4 +10,5 @@ urlpatterns = [
     path('', index_view, name='home'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('delete/', delete_view, name='delete'),
 ]
