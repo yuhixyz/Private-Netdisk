452c140e3bcef8bb5fc334c46d546ac8081d6e72
yuhixyz <byh668972@gmail.com>
2021-01-30 12:18:13 +0800

    更新密钥说明

--- settings/dev.py
+++ settings/dev.py
@@ -1,9 +1,10 @@
 from .base import *
 
-
+# 请将如下密钥进行替换（本人均已替换）
 SECRET_KEY = '&)+ox*a-3v0kzud=628lrp5-(u=s2nsm%b@+on@*fx#j$5i!f^'
 
-
+# 请用腾讯云COS密钥进行替换（本人均已替换）
 COS_SECRET_ID = 'AKIDe7ML3TmhRAXgbGJg0ShXBerVv4wTIMhp'
 COS_SECRET_KEY = 'DDzWj3EC0bz7zOzMagvxhBoh3A1dswnw'
 APPID = '1304853137'
+g'i'
